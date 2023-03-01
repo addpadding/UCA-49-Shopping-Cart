cfb64de11a9d330f5faca72cf0aefa2200df7fb5
addpadding <tiktokaddison20@gmail.com>
2023-03-01 10:05:17 +0300

    Part 3 = java - add => function = draw_cart_ui

--- js/cart_product.js
+++ js/cart_product.js
@@ -1,5 +1,34 @@
 let product_in_cart = localStorage.getItem("productCart_set")
 
 if (product_in_cart) {
-    let item_s = JSON.parse(product_in_cart)
+    let item_s = JSON.parse(product_in_cart);
+    draw_cart_ui(item_s)
+}
+
+
+
+function draw_cart_ui() {
+    let products_ui = product_obj.map((item) => {
+        return `
+    <div class="product_item">
+        <img class="product_item_img" src="${item.imageURL_obj}" alt="">
+
+        <div class="product_item_desc">
+            <h2>${item.title_obj}</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur.
+            </p>
+            <span> size : ${item.size_obj} </span>
+        </div>
+
+        <div class="product_item_actions">
+            <button class="add_to_cart" onclick="addToCart(${item.id_obj})" >Add to Cart</button>
+            <i class="favorite far fa-heart"></i>
+        </div>
+    </div>
+
+        `
+    });
+
+    products_Dom.innerHTML = products_ui;
 }
