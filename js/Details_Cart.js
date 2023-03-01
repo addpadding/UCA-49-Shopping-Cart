8ff10d4d853f487f0827fb9e01e80ce682fc5891
addpadding <tiktokaddison20@gmail.com>
2023-03-01 16:13:20 +0300

    Part 4 = java - add => let product_Details

--- js/Details_Cart.js
+++ js/Details_Cart.js
@@ -1,1 +1,3 @@
-let product_Data_B = JSON.parse(localStorage.getItem("product_Data_B_set"))
+let product_Data_B2 = JSON.parse(localStorage.getItem("product_obj_set"))
+
+let product_Details = product_Data_B2
