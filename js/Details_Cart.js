b0c9f455d6e5a6550cc976cf454666ad5e366ce9
addpadding <tiktokaddison20@gmail.com>
2023-03-01 16:52:23 +0300

    Part 4 = html + css - add => input id=search

--- js/Details_Cart.js
+++ js/Details_Cart.js
@@ -5,4 +5,10 @@ let products_id = localStorage.getItem("product_Id_set")
 let Details_item_Dom = document.querySelector(".item_Details")
 
 let product_Details = product_Data_B2.find((item) => item.id_obj == products_id)
-console.log(product_Details)
+
+
+Details_item_Dom.innerHTML = `
+<img src="${product_Details.imageURL_obj}" alt="">
+<h2> ${product_Details.title_obj} </h2>
+<span> size : ${product_Details.size_obj} </span>
+`
