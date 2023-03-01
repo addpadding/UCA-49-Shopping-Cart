bcda14927673b8ba2dace821aa114953a5f046ff
addpadding <tiktokaddison20@gmail.com>
2023-03-01 16:28:18 +0300

    Part 4 = html - add => item_Details

--- js/Details_Cart.js
+++ js/Details_Cart.js
@@ -2,4 +2,5 @@ let product_Data_B2 = JSON.parse(localStorage.getItem("product_obj_set"))
 
 let products_id = localStorage.getItem("product_Id_set")
 
-let product_Details = product_Data_B2.map((item) => item.id_obj == products_id)
+let product_Details = product_Data_B2.find((item) => item.id_obj == products_id)
+console.log(product_Details)
