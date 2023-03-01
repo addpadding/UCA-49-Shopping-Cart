41c57f9f3d2a68d297cfda630c3d13cccbba7553
addpadding <tiktokaddison20@gmail.com>
2023-03-01 16:37:07 +0300

    Part 4 = js - add => let Details_item_Dom

--- js/Details_Cart.js
+++ js/Details_Cart.js
@@ -2,5 +2,7 @@ let product_Data_B2 = JSON.parse(localStorage.getItem("product_obj_set"))
 
 let products_id = localStorage.getItem("product_Id_set")
 
+let Details_item_Dom = document.querySelector(".item_Details")
+
 let product_Details = product_Data_B2.find((item) => item.id_obj == products_id)
 console.log(product_Details)
