4da0dcb0b951e1be4d00ab2111944e6cc31e4521
addpadding <tiktokaddison20@gmail.com>
2023-03-03 09:38:31 +0300

    Part 6 = js - add => let create_Name + let create_Form

---
+++ js/create_plus.js
@@ -0,0 +1,9 @@
+// create_plus
+console.log("create_plus")
+
+
+// variables
+let create_Name = document.getElementById("create_product_name")
+let create_Desc = document.getElementById("create_product_desc")
+let create_Siza = document.getElementById("create_product_size")
+let create_Form = document.getElementById("create_product_form")
