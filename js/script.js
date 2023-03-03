fb545cd0c3de5a94d95a9fefc5149a2a9941efdc
addpadding <tiktokaddison20@gmail.com>
2023-03-03 14:40:21 +0300

    Part 7 = 07:40 = js - add => let product

--- js/script.js
+++ js/script.js
@@ -69,6 +69,9 @@ if (add_item) {
 // add to cart
 function addToCart(id_item) {
     if (localStorage.getItem("username_set")) {
+
+        let product = JSON.parse(localStorage.getItem("product_obj_set")) || ppp;
+
         let product__choosen = product_Data_B.find(
             (find_item) => find_item.id_obj === id_item
         );
