5e87a929c1304382ca10d3a0c1db62538467fcd6
addpadding <tiktokaddison20@gmail.com>
2023-03-03 09:42:23 +0300

    Part 6 = js - add => function get_product_size_value

--- js/create_plus.js
+++ js/create_plus.js
@@ -1,9 +1,12 @@
 // create_plus
-console.log("create_plus")
-
+console.log("create_plus");
 
 // variables
-let create_Name = document.getElementById("create_product_name")
-let create_Desc = document.getElementById("create_product_desc")
-let create_Siza = document.getElementById("create_product_size")
-let create_Form = document.getElementById("create_product_form")
+let create_Name = document.getElementById("create_product_name");
+let create_Desc = document.getElementById("create_product_desc");
+let create_Siza_Select = document.getElementById("create_product_size");
+let create_Form = document.getElementById("create_product_form");
+
+create_Siza_Select.addEventListener("change", get_product_size_value)
+
+function get_product_size_value(e) { }
