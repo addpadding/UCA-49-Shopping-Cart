85b98329a88af376ebdf459fec721eb94e89b4ab
addpadding <tiktokaddison20@gmail.com>
2023-03-04 14:28:55 +0300

    Part 7 = js - changes => productsMain {product_Data_B}

--- js/Details_Cart.js
+++ js/Details_Cart.js
@@ -1,10 +1,10 @@
-let product_Data_B2 = JSON.parse(localStorage.getItem("product_obj_set"))
+let productsMain2 = JSON.parse(localStorage.getItem("product_obj_set"))
 
 let products_id = localStorage.getItem("product_Id_set")
 
 let Details_item_Dom = document.querySelector(".item_Details")
 
-let product_Details = product_Data_B2.find((item) => item.id_obj == products_id)
+let product_Details = productsMain2.find((item) => item.id_obj == products_id)
 
 
 Details_item_Dom.innerHTML = `
