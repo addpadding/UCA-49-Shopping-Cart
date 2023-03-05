ccd39140e7750c5cd52815956b7ea23a09bab62c
addpadding <tiktokaddison20@gmail.com>
2023-03-05 18:41:12 +0300

    Part 8 = js - add => function draw_My_product

--- js/MY__product.js
+++ js/MY__product.js
@@ -8,3 +8,37 @@ let products_Storage = JSON.parse(localStorage.getItem("products_Data_set") || p
 // filter
 let MY__product = products_Storage.filter(item => item.isMe_obj === "Y")
 console.log("MY__product update", MY__product)
+
+
+// draw_My_product
+function draw_My_product(all_product = []) {
+
+    if (JSON.parse(localStorage.getItem("productCart_set")).length === 0) {
+        no_products_Dom.innerHTML = "no item"
+    }
+
+    let product_item =
+        JSON.parse(localStorage.getItem("productCart_set")) || all_product
+
+    let products_ui = product_item.map((item) => {
+        return `
+    <div class="product_item">
+        <img class="product_item_img" src="${item.imageURL_obj}" alt="">
+
+        <div class="product_item_desc">
+            <h2>${item.title_obj}</h2>
+            <p>${item.desc_obj}</p>
+            <span> size : ${item.size_obj} </span> <br>
+            <span> Quntatit : ${item.qty_obj} </span>
+        </div>
+
+        <div class="product_item_actions">
+            <button class="add_to_cart" onclick="remove_item_from_Cart(${item.id_obj})" >Remove from Cart</button>
+        </div>
+    </div>
+
+        `
+    });
+
+    products_Dom_2.innerHTML = products_ui.join("");
+}
