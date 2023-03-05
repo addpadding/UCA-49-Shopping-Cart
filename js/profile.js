49e7a87bd53d7a9ea00362505ea74962602e0a37
addpadding <tiktokaddison20@gmail.com>
2023-03-06 01:15:32 +0300

    Part 9 = js - add => let products_Storage +  let MY__product

--- js/profile.js
+++ js/profile.js
@@ -4,7 +4,12 @@ console.log("profile")
 // Get Data From local Storage
 let username_Storage = localStorage.getItem("username_set");
 let email_Storage = localStorage.getItem("email_set");
+let products_Storage = JSON.parse(localStorage.getItem("products_Data_set") || products_Data_obj);
+let MY__product = products_Storage.filter(item => item.isMe_obj === "Y");
 
 // variables
-let username_profile = document.getElementById("username_profile")
-let email_profile = document.getElementById("username_profile")
+let username_profile_DOM = document.getElementById("username")
+let email_profile_DOM = document.getElementById("email")
+
+username_profile_DOM.innerHTML = username_Storage
+email_profile_DOM.innerHTML = email_Storage
