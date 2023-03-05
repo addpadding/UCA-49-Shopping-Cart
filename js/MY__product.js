7774dbb4090f9ae446c8304214faa95ec843c276
addpadding <tiktokaddison20@gmail.com>
2023-03-05 18:38:24 +0300

    Part 8 = js - add => let MY__product

--- js/MY__product.js
+++ js/MY__product.js
@@ -2,6 +2,9 @@
 console.log("MY__product");
 
 
-// variables
+// localStorage
 let products_Storage = JSON.parse(localStorage.getItem("products_Data_set") || products_Data_obj)
-console.log(products_Storage)
+
+// filter
+let MY__product = products_Storage.filter(item => item.isMe_obj === "Y")
+console.log("MY__product update", MY__product)
