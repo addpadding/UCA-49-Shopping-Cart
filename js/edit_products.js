69814712e6faba2f30f5f0baa98c9a600107142e
addpadding <tiktokaddison20@gmail.com>
2023-03-05 15:27:21 +0300

    Part 7 = css - changes => function update_Product_Fun

--- js/edit_products.js
+++ js/edit_products.js
@@ -6,6 +6,7 @@ console.log("edit_products");
 let products_Storage = JSON.parse(localStorage.getItem("products_Data_set") || products_Data_obj)
 let id_pro_Storage = JSON.parse(localStorage.getItem("EditProduct_set"))
 
+// find
 let get_product = products_Storage.find(item => item.id_obj === id_pro_Storage)
 
 
@@ -18,93 +19,68 @@ let input_File = document.getElementById("upload_img_file");
 let products_size_value;
 let products_image;
 
-product_Name.value = get_product.title_obj
-product_Desc.value = get_product.desc_obj
-product_Siza_Select.value = get_product.size_obj
+product_Name.value = get_product.title_obj;
+product_Desc.value = get_product.desc_obj;
+
+product_Siza_Select.value = get_product.size_obj;
+products_image = get_product.imageURL_obj;
 
 // Events
 product_Siza_Select.addEventListener("change", get_product_size_value);
-create_Form.addEventListener("submit", update_Product_Fun);
-// input_File.addEventListener("change", upload_image);
+update_Form.addEventListener("submit", update_Product_Fun);
+input_File.addEventListener("change", upload_image);
 
 // Functions
 function get_product_size_value(e) {
     products_size_value = e.target.value;
 }
 
+// update function
 function update_Product_Fun(e) {
-    //     e.preventDefault();
+    e.preventDefault();
 
-    //     let all_product_storage = JSON.parse(localStorage.getItem("products_Data_set")) || products_Data_obj;
-    //     let name_value = product_Name.value;
-    //     let desc_value = product_Desc.value;
+    get_product.title_obj = product_Name.value
+    get_product.desc_obj = product_Desc.value
 
-    //     if (name_value && desc_value) {
+    get_product.size_obj = products_size_value
+    get_product.imageURL_obj = products_image
 
-    //         let Create_Obj = {
 
-    //             id_obj: all_product_storage ? all_product_storage.length + 1 : 1,
-
-    //             title_obj: name_value,
-
-    //             desc_obj: desc_value,
-
-    //             size_obj: products_size_value,
-
-    //             imageURL_obj: products_image,
-
-    //             qty_obj: 1,
-
-    //             isMe_obj: "Y",
-    //         };
-
-    //         let new_product_Storage = all_product_storage ? [...all_product_storage, Create_Obj] : [Create_Obj];
-    //         localStorage.setItem("products_Data_set", JSON.stringify(new_product_Storage));
-
-    //         product_Name.value = "";
-    //         product_Desc.value = "";
-    //         product_Siza_Select.value = "";
-
-    //         setTimeout(() => {
-    //             window.location = "index.html"
-    //         }, 500);
-
-    //     } else {
-    //         alert("Enter Data ..")
-    //     }
-
+    localStorage.setItem("products_Data_set", JSON.stringify(products_Storage));
 }
 
-// // upload_image
-// function upload_image() {
-//     let file_e = this.files[0];
+// upload_image
+function upload_image() {
+    let file_e = this.files[0];
 
-//     let type_s = ["image/jpeg", "image/png"]
+    let type_s = ["image/jpeg", "image/png"]
 
-//     if (type_s.indexOf(file_e.type) == -1) {
-//         alert("type not support")
-//         return;
-//     }
+    if (type_s.indexOf(file_e.type) == -1) {
+        alert("type not support")
+        return;
+    }
 
-//     if (file_e.size > 2 * 1024 * 1024) {
-//         alert("img not Exced 2MG")
-//         return;
-//     }
+    if (file_e.size > 2 * 1024 * 1024) {
+        alert("img not Exced 2MG")
+        return;
+    }
 
-//     get_img_Base_64(file_e);
-// }
+    get_img_Base_64(file_e);
+}
 
-// function get_img_Base_64(file_item) {
-//     let reader = new FileReader()
 
-//     reader.readAsDataURL(file_item)
 
-//     reader.onload = function () {
-//         products_image = reader.result
-//     };
+function get_img_Base_64(file_item) {
+    let reader = new FileReader()
 
-//     reader.onerror = function () {
-//         alert("Error !!");
-//     };
+    reader.readAsDataURL(file_item)
 
-// }
+    reader.onload = function () {
+        products_image = reader.result
+    };
+
+    reader.onerror = function () {
+        alert("Error !!");
+    };
+
+}
