8865e296e20fe223fb58d3f28d40ba4f9593b6a5
addpadding <tiktokaddison20@gmail.com>
2023-03-06 01:00:52 +0300

    Part 9 = js - add => let username_Storage + let username_profile

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,10 @@
 // profile
 console.log("profile")
 
+// Get Data From local Storage
+let username_Storage = localStorage.getItem("username_set");
+let email_Storage = localStorage.getItem("email_set");
 
-let username_profile = localStorage.getItem("username_set");
-let email_profile = localStorage.getItem("email_set");
+// variables
+let username_profile = document.getElementById("username_profile")
+let email_profile = document.getElementById("username_profile")
