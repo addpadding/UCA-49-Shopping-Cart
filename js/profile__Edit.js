601c8693b06126d73351f22563e38f4544460966
addpadding <tiktokaddison20@gmail.com>
2023-03-06 13:06:44 +0300

    Part 9 = js - add => function Data_Edit_profile

--- js/profile__Edit.js
+++ js/profile__Edit.js
@@ -1,2 +1,20 @@
 // profile__Edit
 console.log("profile__Edit")
+
+// Get Data From local Storage
+let username_Storage2 = localStorage.getItem("username_set");
+let email_Storage2 = localStorage.getItem("email_set");
+
+// variables
+let username_Edit_profile = document.getElementById("username")
+let email_Edit_profile = document.getElementById("email")
+let form_Edit_profile = document.getElementById("profile__Edit_form")
+
+// setting value of input
+username_Edit_profile.value = username_Storage2;
+email_Edit_profile.value = email_Storage2;
+
+// Events
+form_Edit_profile.addEventListener("submit", Data_Edit_profile)
+
+function Data_Edit_profile(item) { }
