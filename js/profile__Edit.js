f5ff1948dd2721b05149e949fa42f8c073490f00
addpadding <tiktokaddison20@gmail.com>
2023-03-06 13:28:39 +0300

    Part 9 = js - add => setTimeout

--- js/profile__Edit.js
+++ js/profile__Edit.js
@@ -6,8 +6,8 @@ let username_Storage2 = localStorage.getItem("username_set");
 let email_Storage2 = localStorage.getItem("email_set");
 
 // variables
-let username_Edit_profile = document.getElementById("username")
-let email_Edit_profile = document.getElementById("email")
+let username_Edit_profile = document.getElementById("chanage_Name")
+let email_Edit_profile = document.getElementById("chanage_Email")
 let form_Edit_profile = document.getElementById("profile__Edit_form")
 
 // setting value of input
@@ -17,10 +17,13 @@ email_Edit_profile.value = email_Storage2;
 // Events
 form_Edit_profile.addEventListener("submit", Data_Edit_profile)
 
-function Data_Edit_profile(item) {
+function Data_Edit_profile(e) {
     e.preventDefault();
 
-    localStorage.setItem("username_Edit_profile_set", username_Edit_profile.value)
-    localStorage.setItem("email_Edit_profile_set", email_Edit_profile.value)
+    localStorage.setItem("username_set", username_Edit_profile.value)
+    localStorage.setItem("email_set", email_Edit_profile.value)
 
+    setTimeout(() => {
+        window.location = "profile.html"
+    }, 500);
 }
